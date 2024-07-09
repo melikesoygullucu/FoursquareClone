8f3ebee078df04724c35e3cba380390354286d5a
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-09 17:24:47 +0300

    save and fetch data from parse

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       /* let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
+        parseObject.saveInBackground { succes, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                print("uploaded")
+            }
+        }*/
+        
+        let query = PFQuery(className: "Fruits")
+        query.whereKey("calories", equalTo: 150)
+        query.findObjectsInBackground { objects, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                print(objects)
+            }
+        }
     }
 
 
