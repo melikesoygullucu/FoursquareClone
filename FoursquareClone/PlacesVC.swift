90f10e884286b8be70daff4534f3ae0e867cf5df
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-11 13:25:10 +0300

    logout button and function added

--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesVC: UIViewController {
 
@@ -14,10 +15,30 @@ class PlacesVC: UIViewController {
         super.viewDidLoad()
 
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logout))
     }
     
     @objc func addButton () {
         
+    }
+    
+    @objc func logout () {
+        PFUser.logOutInBackground { error in
+            if error != nil {
+                
+            } else {
+                self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
+            }
+        }
+        
+    }
+    
+    func alertDialog(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
--- FoursquareClone/SignUpVC.swift
+++ FoursquareClone/SignUpVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class SignUpVC: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
