b2bc20f139f1369b11d1aec66aad2da76ac194a7
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-13 23:04:44 +0300

    DetailsVC design and class added

---
+++ FoursquareClone/DetailsVC.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsVC.swift
+//  FoursquareClone
+//
+//  Created by Melike Soygüllücü on 13.07.2024.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var detailsImageView: UIImageView!
+    @IBOutlet weak var detailsNameLabel: UILabel!
+    @IBOutlet weak var detailsTypeLabel: UILabel!
+    @IBOutlet weak var detailsExplanationLabel: UILabel!
+    @IBOutlet weak var detailsMapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
