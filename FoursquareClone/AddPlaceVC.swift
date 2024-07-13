35267594ac78b4901240b036a7b8f800a0311793
melikesoygullucu <melikesoygul@hotmail.com>
2024-07-13 23:22:25 +0300

    Gesture recognizer for image selection added

--- FoursquareClone/AddPlaceVC.swift
+++ FoursquareClone/AddPlaceVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddPlaceVC: UIViewController {
+class AddPlaceVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var placeNameTextField: UITextField!
@@ -20,7 +20,16 @@ class AddPlaceVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        placeImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        placeImageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true)
     }
     
     @IBAction func nextButton(_ sender: Any) {
